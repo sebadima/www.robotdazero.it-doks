68c081d2e277082ec2a665dbff4e0517266d1eed
sebadima <lenovo@sebadima.com>
2023-07-17 18:43:41 +0200

    test CF

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,6 +1,35 @@
-/**
- * POST /api/submit
- */
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+const log = console.log;
+
+var mailgun_user    = process.env.MAILGUN_USER;
+var mailgun_api_key = process.env.MAILGUN_API_KEY;
+var mailgun_from    = process.env.MAILGUN_FROM;
+var mailgun_to      = process.env.MAILGUN_TO;
+
+console.log("utente: %s", mailgun_user);
+
+async function main() {
+ let transporter = nodemailer.createTransport({
+   host: "smtp.eu.mailgun.org",
+   port: 587,
+   auth: {
+     user: mailgun_user,
+     pass: mailgun_api_key,
+   },
+ });
+
+ let info = await transporter.sendMail({
+   from: mailgun_from,
+   to: mailgun_to,
+   subject: "ore da CF1629Hello",
+   text: "ciao ciao Testing some Mailgun awesomness"
+ });
+
+ console.log("Message sent: %s", info.messageId);
+}
+
+main().catch(console.error);
 
 export async function onRequestPost(context) {
   try {
